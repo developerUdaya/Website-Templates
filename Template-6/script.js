03d123fc2398011723daf5de9a278e321eab10a5
Giri Naveen <132736158+Giri-Naveen@users.noreply.github.com>
2025-05-16 14:26:37 +0530

    Update script.js

--- Template-6/script.js
+++ Template-6/script.js
@@ -1,1 +1,34 @@
+// Sticky navbar logic
+const stickyNavbar = document.getElementById("sticky-navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    stickyNavbar.classList.add("visible");
+  } else {
+    stickyNavbar.classList.remove("visible");
+  }
+});
 
+// Accordion logic
+document.querySelectorAll(".accordion-button").forEach(button => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("active");
+    const content = button.nextElementSibling;
+    content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + "px";
+  });
+});
+
+//mobile-navbar
+const openMenuBtn = document.getElementById('openMenu');
+const closeMenuBtn = document.getElementById('closeMenu');
+const mobileMenu = document.getElementById('mobileMenu');
+const body = document.body;
+
+openMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('active');
+  body.style.overflow = 'hidden';
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+  body.style.overflow = '';
+});
